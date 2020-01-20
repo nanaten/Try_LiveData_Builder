384dcf9d074c3d8b28baf6e019431c5855226695
m.coder <kurokoshow00@gmail.com>
2020-01-20 10:05:43 +0900

    add normal livedata sample

--- app/src/main/java/com/nanaten/try_livedata_builder/MainViewModel.kt
+++ app/src/main/java/com/nanaten/try_livedata_builder/MainViewModel.kt
@@ -4,10 +4,12 @@ import android.util.Log
 import androidx.lifecycle.*
 import com.nanaten.try_livedata_builder.api.ApiService
 import com.nanaten.try_livedata_builder.entity.Repos
+import kotlinx.coroutines.launch
 import java.lang.Exception
 
 class MainViewModel : ViewModel() {
     val api = ApiService.get()
+
     // LiveData Builder
     val items: LiveData<List<Repos>> = liveData {
         try {
@@ -17,6 +19,21 @@ class MainViewModel : ViewModel() {
         catch (e: Exception) {
             Log.d("Error", "${e.message}")
             emit(listOf())
+        }
+    }
+
+    // Normal LiveData
+    // No Use
+    val mutableData = MutableLiveData<List<Repos>>()
+    fun getRepos() {
+        viewModelScope.launch {
+            try {
+                val repos = api.getRepos()
+                mutableData.postValue(repos)
+            }
+            catch (e: Exception) {
+                Log.d("Error", "${e.message}")
+            }
         }
     }
 }
